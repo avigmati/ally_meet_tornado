87ebbe31c4a69246344aca8f76d28a49f68920e0
avigmati <avigmati@gmail.com>
2015-11-18 03:10:52 +0300

    cleaned prepare(), added MAX_BUFFER_SIZE, small fixes

--- server.py
+++ server.py
@@ -5,22 +5,19 @@ import os
 import uuid
 import re
 from tornado.web import RequestHandler, stream_request_body, asynchronous, gen
+#
 # import tornado.stack_context
+#
+# Неясно пока для чего эта бандура
+#
+##############################
 import logging
 from tornado.options import define, options
 
 
 _UPLOAD_PATH = './upload'
-UPLOADS_KEYS = {}
-
-
-def _run_once(f):
-    def wrapper(*args, **kwargs):
-        if not wrapper.has_run:
-            wrapper.has_run = True
-            return f(*args, **kwargs)
-    wrapper.has_run = False
-    return wrapper
+UPLOAD_KEYS = []
+MAX_BUFFER_SIZE = 7813988580 # allowed file size in bytes
 
 
 class UploadForm(RequestHandler):
@@ -30,29 +27,20 @@ class UploadForm(RequestHandler):
 
 class UploadFile():
 
-    @gen.coroutine
     def __init__(self, request):
+        self.request = request
         self.filename = uuid.uuid4().hex
         self.filepath = os.path.join(_UPLOAD_PATH, self.filename)
-        try:
-            self.content_length = int(request.headers.get('Content-Length'))
-        except Exception as e:
-            # logging.error('Exception "{0}" occurred while reading header "Content-Length"!'.format(str(e)))
-            raise('Exception "{0}" occurred while reading header "Content-Length"!'.format(str(e)))
-
         self.original_filename = ''
         self.content_type = ''
         self.read_bytes = 0
         self.chunk_number = 0
         self.file = open(self.filepath, 'wb')
-
-    @gen.coroutine
-    def write(self, chunk):
-        self.file.write(chunk)
-
-    @gen.coroutine
-    def close(self):
-        self.file.close()
+        try:
+            self.content_length = int(self.request.headers.get('Content-Length'))
+        except Exception as e:
+            # logging.error('Exception "{0}" occurred while reading header "Content-Length"!'.format(str(e)))
+            raise('Exception "{0}" occurred while reading header "Content-Length"!'.format(str(e)))
 
 
 @stream_request_body
@@ -60,62 +48,49 @@ class Upload(RequestHandler):
 
     @gen.coroutine
     def prepare(self):
-        filename = uuid.uuid4().hex
-        filepath = os.path.join(_UPLOAD_PATH, filename)
-        try:
-            self.file = open(filepath, 'wb')
-        except Exception as e:
-            logging.error('Exception "{0}" occurred while opening destination file: "{1}".\n'
-                          'Traceback:'
-                          .format(str(e)), filepath)
-            raise tornado.web.HTTPError(404)
-        try:
-            self.content_length = int(self.request.headers.get('Content-Length'))
-        except Exception as e:
-            logging.error('Exception "{0}" occurred while reading header "Content-Length".\n'
-                          'Traceback:'
-                          .format(str(e)))
-            raise tornado.web.HTTPError(404)
-
-        self.original_filename = ''
-        self.content_type = ''
-        self.read_bytes = 0
-        self.chunk_number = 0
-
-        UPLOADS_KEYS[filename] = {'content_length': self.content_length, 'original_filename': self.original_filename,
-                                  }
+        self.file = UploadFile(self.request)
+        UPLOAD_KEYS.append(self.file.filename)
 
     @gen.coroutine
     def post(self):
         # self.finish()
         print('ok:')
-        print(UPLOADS_KEYS)
+        print(UPLOAD_KEYS)
 
     @gen.coroutine
     def data_received(self, chunk):
-        self.read_bytes += len(chunk)
-        self.chunk_number += 1
+        self.file.read_bytes += len(chunk)
+        self.file.chunk_number += 1
 
-        if self.chunk_number == 1:
+        if self.file.chunk_number == 1:
             try:
                 chunk = yield self._get_head(chunk)
             except Exception as e:
                 logging.error('Exception "{0}" occurred while parsing first chunk.\n'
                               'Readed: {1} bytes.\nTraceback:'
-                              .format(str(e), self.read_bytes))
+                              .format(str(e), self.file.read_bytes))
                 raise tornado.web.HTTPError(500)
 
-        if self.read_bytes == self.content_length:
+        if self.file.read_bytes == self.file.content_length:
             try:
                 chunk = yield self._clear_end(chunk)
+
             except Exception as e:
                 logging.error('Exception "{0}" occurred while parsing last chunk.\n'
                               'Readed: {1} bytes.\nTraceback:'
-                              .format(str(e), self.read_bytes))
+                              .format(str(e), self.file.read_bytes))
                 raise tornado.web.HTTPError(500)
-            yield self._finish()
 
-        self.file.write(chunk)
+        self.file.file.write(chunk)
+
+        if self.file.read_bytes == self.file.content_length:
+            self.file.chunk_number = 0
+            self.file.file.close()
+            newname = '{}_{}'.format(self.file.filename, self.file.original_filename)
+            os.rename(self.file.filepath, os.path.join(_UPLOAD_PATH, newname))
+            msg = 'Uploaded: {} "{}", {} bytes'.format(newname, self.file.content_type, self.file.content_length)
+            logging.info(msg)
+            print(msg)
 
     @gen.coroutine
     def _get_head(self, chunk):
@@ -125,9 +100,9 @@ class Upload(RequestHandler):
         """
         head_arr = chunk.partition(b'\r\n\r\n')
         name_match = re.match('.* filename="([^"]*)"', str(head_arr[0]))
-        self.original_filename = name_match.group(1)
+        self.file.original_filename = name_match.group(1)
         ct_match = re.match('.*\s*Content-Type: (.*)\'', str(head_arr[0]))
-        self.content_type = ct_match.group(1)
+        self.file.content_type = ct_match.group(1)
         chunk = head_arr[2]  # replace with clean body
 
         # raise ValueError('Wrong filename!')
@@ -135,20 +110,10 @@ class Upload(RequestHandler):
 
     @gen.coroutine
     def _clear_end(self, chunk):
-        if self.content_length == self.read_bytes:
+        if self.file.content_length == self.file.read_bytes:
             end_arr = chunk[::-1].partition(b'------\n\r')  # reverse for partition()
             chunk = end_arr[2][::-1]  # replace with clean body
         return chunk
-
-    @gen.coroutine
-    def _finish(self):
-        self.chunk_number = 0
-        self.file.close()
-        newname = '{}_{}'.format(self.file.name, self.original_filename)
-        os.rename(self.file.name, newname)
-        msg = 'Uploaded: {} "{}", {} bytes'.format(newname, self.content_type, self.content_length)
-        logging.info(msg)
-        print(msg)
 
 
 if __name__ == "__main__":
@@ -158,5 +123,5 @@ if __name__ == "__main__":
     ], debug=True)
 
     opts = tornado.options.parse_command_line()
-    app.listen(8888)
+    app.listen(8888, max_buffer_size=MAX_BUFFER_SIZE)
     tornado.ioloop.IOLoop.instance().start()
